fea99786e45476dc4310f88cf6e09c7b95ee5a1a
LauraOrozco1320 <orozcolauracamila@gmail.com>
2024-09-08 21:16:42 -0500

    first commit

---
+++ 6. Listener/app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+
+container.addEventListener("mouseover", () =>{
+    container.style.backgroundColor = "blue";
+});
+
+container.addEventListener("mouseout", () =>{
+    container.style.backgroundColor = "red";
+});
+
+/*button.addEventListener("click", () =>{
+    alert("Boton presionado");
+});*/
+
+const buttonClickCallback = () => {
+    alert("Button Clicked!");
+  };
+  
+  button.addEventListener("click", buttonClickCallback);
+  
+  setTimeout(() => {
+    button.removeEventListener("click", buttonClickCallback);
+  }, 2000);
---
+++ 7. EventObject/app.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+
+const buttonClicked = (event) =>{
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.id);
+    console.log(event.target.textContent);
+    
+};
+
+button.addEventListener("click", buttonClicked);
